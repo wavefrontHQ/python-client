ce9cc8624dc5e87a8f4125c28976317e29ec0a8c
Artem Ustinov <artem.ustinov@gmail.com>
2019-03-22 17:00:50 -0700

    Update .travis.yml and requirements.txt

--- setup.py
+++ setup.py
@@ -22,7 +22,12 @@ VERSION = "2.9.39"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+REQUIRES = [
+    "certifi>=2017.4.17",
+    "python-dateutil>=2.1",
+    "six>=1.10",
+    "urllib3>=1.23"
+]
 
 setup(
     name=NAME,
