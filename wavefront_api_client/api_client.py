7b2dd29761a742f27dad100c01ac4615da178926
Artem Ustinov <artem.ustinov@gmail.com>
2019-09-05 16:35:38 -0700

    Autogenerated Update v2.35.34.

--- wavefront_api_client/api_client.py
+++ wavefront_api_client/api_client.py
@@ -74,7 +74,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/2.35.25/python'
+        self.user_agent = 'Swagger-Codegen/2.35.34/python'
 
     def __del__(self):
         if self._pool is not None:
--- wavefront_api_client/configuration.py
+++ wavefront_api_client/configuration.py
@@ -240,5 +240,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: 2.35.25".\
+               "SDK Package Version: 2.35.34".\
                format(env=sys.platform, pyversion=sys.version)
