715d16f42d5e9b16716813cef64e630e3a1b9d6b
wf-jenkins <svc.wf-jenkins@vmware.com>
2020-10-13 08:16:26 -0700

    Autogenerated Update v2.63.18.

--- wavefront_api_client/configuration.py
+++ wavefront_api_client/configuration.py
@@ -248,5 +248,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: 2.63.16".\
+               "SDK Package Version: 2.63.18".\
                format(env=sys.platform, pyversion=sys.version)
