d6cf914934d47c9b236cdcfd7cd427624fa6d265
wf-jenkins <svc.wf-jenkins@vmware.com>
2021-01-25 08:16:27 -0800

    Autogenerated Update v2.65.30.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "wavefront-api-client"
-VERSION = "2.65.27"
+VERSION = "2.65.30"
 # To install the library, run the following
 #
 # python setup.py install
