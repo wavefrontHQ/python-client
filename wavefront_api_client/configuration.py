5930610e72a0db1d47e3aec35781c70db6d97e4b
wf-jenkins <svc.wf-jenkins@vmware.com>
2021-06-29 08:16:32 -0700

    Autogenerated Update v2.91.2.

--- wavefront_api_client/configuration.py
+++ wavefront_api_client/configuration.py
@@ -248,5 +248,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: 2.88.5".\
+               "SDK Package Version: 2.91.2".\
                format(env=sys.platform, pyversion=sys.version)
