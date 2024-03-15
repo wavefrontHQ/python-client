03c14ca4561592214b616fdd46cf87e556d60343
wf-jenkins <svc.tobs-eso@vmware.com>
2024-03-15 08:16:19 -0700

    Autogenerated Update v2.219.6.

--- wavefront_api_client/api_client.py
+++ wavefront_api_client/api_client.py
@@ -74,7 +74,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/2.218.0/python'
+        self.user_agent = 'Swagger-Codegen/2.219.6/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __del__(self):
